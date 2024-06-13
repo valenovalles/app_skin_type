a797f57bd127ec198d5bd27dcc36ef4a6bbcfd35
Valentina <valenovalles@gmail.com>
2024-06-13 13:50:05 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../images/skin_logo.png"
 function Header() {
   return (
     <div>
-        <a href="/"><img className= "logo" src={logo} alt="" /></a>
+        <a href="/app_skin_type/"><img className= "logo" src={logo} alt="" /></a>
     </div>
   )
 }
